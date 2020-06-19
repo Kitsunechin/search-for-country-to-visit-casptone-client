47d5d218cd0d5495534db1f8ea26b068fb856be0
Martyna Bulska <martynabulska@Martynas-MacBook-Pro-2.local>
2020-06-19 11:09:04 +0200

    adding validation

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,17 +5,77 @@ export default class LoginPage extends React.Component {
 
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        error: null,
+        params: {
+          username: '',
+          email: '',
+          password: ''
+        }
+      };
+    }
+
+    validateUsername(inputUsername){
+      let outputUsername = inputUsername;
+      // only lowercase and uppercase letters and dash
+      let userformat = /^[a-zA-Z\-]+$/;
+      if(!inputUsername.match(userformat)) {
+          outputUsername = ""
+      }
+      return outputUsername
+    }
+
+    validatePassword(inputPassword){
+      let outputPassword = inputPassword;
+      // at least one number, one lowercase and one uppercase letter
+      // at least eight characters that are letters, numbers or the underscore
+      let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
+      if(!inputPassword.match(passwordformat)) {
+          outputPassword = ""
+      }
+      return outputPassword
     }
   
     handleSubmit = (e) => {
       e.preventDefault();
+      //create an object to store the search filters
+      const data = {}
+
+      //get all the from data from the form component
+      const formData = new FormData(e.target)
+
+      //for each of the keys in form data populate it with form value
+      for (let value of formData) {
+          data[value[0]] = value[1]
+      }
+      console.log(data)
+      let {username, password} = data
+    
+      if (this.validateUsername(username) === '') {
+        this.setState({
+            error: 'username is not valid'
+        })
+      }
+      if (this.validatePassword(password) === '') {
+        this.setState({
+            error: 'password is not valid'
+        })
+      }
+      //assigning the object from the form data to params in the state
+      this.setState({
+          params: data
+      })
+
+      //check if the state is populated with the search params data
+      console.log(this.state.params)
     }
   
     render() {
+      const errorMessage = this.state.error ? <p className="error-message">{this.state.error}</p> : false
       return (
         <div className="inner-container">
           <form className="add-user" onSubmit={this.handleSubmit}>
+          {errorMessage}
           <div className="box">
           
             <div className="input-group">
